35d783f8b2f8a0efe9c8e1eeed9d8800916e01aa
Louis-Guillaume MORAND <lgm@lgmorand.com>
2021-02-27 11:22:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,4 @@
 
 set -e
 
-if ! [[ -z "${OUT_DIR}" ]]; then
-  echo "Creating output directory $OUT_DIR..."
-  mkdir --parent $OUT_DIR
-fi
-
-# "args" from main.workflow get append to below call
 sh -c "pandoc $*"
